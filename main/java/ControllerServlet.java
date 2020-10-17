43d439171d3c3c0dcafd2c45cbad905eba3e15be
MIkEKun0 <58865357+MIkEKun0@users.noreply.github.com>
2020-10-17 21:28:35 +0300

    Add files via upload

---
+++ main/java/ControllerServlet.java
@@ -0,0 +1,36 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ControllerServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        ServletContext servletContext = getServletContext();
+        RequestDispatcher dispatcher;
+        String x = req.getParameter("X");
+        String y = req.getParameter("Y");
+        String r = req.getParameter("R");
+        if (req.getParameterMap().size() == 3 && !x.isEmpty()  && !y.isEmpty() && !r.isEmpty()) {
+            dispatcher = servletContext.getRequestDispatcher("/AreaCheckServlet");
+        } else {
+            dispatcher = servletContext.getRequestDispatcher("/index.jsp");
+        }
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        ServletContext servletContext = getServletContext();
+        RequestDispatcher dispatcher;
+        dispatcher = servletContext.getRequestDispatcher("/index.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
